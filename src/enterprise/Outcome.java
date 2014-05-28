8c9dd9c036f1da51fb5ac7ce2677d6e00d4de495
PabloBrubeck <pbrubeck95@gmail.com>
2014-05-28 12:43:18 -0500

    Sebas es un pendejo

--- src/enterprise/Outcome.java
+++ src/enterprise/Outcome.java
@@ -1,6 +1,38 @@
 package enterprise;
 public class Outcome {
-    float amount;
-    String description;
-    long date;
+    private float amount;
+    private String description;
+    private long date;
+    
+    public Outcome(float f, String d, long l){
+        amount=f;
+        description=d;
+        date=l;
+    }
+    public Outcome(float f, String d){
+       this(f, d, System.currentTimeMillis());
+    }
+    
+    public float getAmount(){
+        return amount;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public long getDate(){
+        return date;
+    }
+    
+    public void setAmount(float f){
+        amount=f;
+    }
+    public void setDescription( String d){
+        description=d;
+    }
+    public void setDate(long l){
+        date=l;
+    }
+    public void setDate(){
+        date=System.currentTimeMillis();
+     }
 }
